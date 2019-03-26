c0e3e5def1bd7898bfbc4ee3fa1f0588b48b50e5
J. Fernando González <jfgonzalezm@gmail.com>
2019-03-26 10:23:42 +0100

    Test Refactor including helper function

--- functional_tests.py
+++ functional_tests.py
@@ -13,6 +13,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # User History
         # Paul has heard about a cool ne online to-do app. He goes
@@ -41,10 +46,9 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
 
-        self.assertTrue(
-            any(row.text == '1: Buy peacok feathers' for row in rows),
-            f"New to-do item did not appear in table. Contents were:\n{table.text}"
-        )
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        
         # There is still a text box inviting her to add another item. She enters 
         # "Use peacock feathers to make a fly"
 
